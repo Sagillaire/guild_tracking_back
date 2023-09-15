aef820e676b2a53962e1640fed4a47a0abff7b80
Sagillaire <Jasantise@outlook.es>
2023-09-14 23:53:52 -0500

    Feature: Add trycatch validation to map_route controllers

--- src/controllers/map_route.controller.ts
+++ src/controllers/map_route.controller.ts
@@ -2,18 +2,30 @@ import { Request, Response } from "express";
 import { getAllMapRouteService, createMapRouteService, getMapRouteByIdService } from "../services/map_route.service";
 
 const getByIdController = async ({ params }: Request, res: Response) => {
-    const response = await getMapRouteByIdService(params?.id)
-    res.status(200).json(response)
+    try {
+        const response = await getMapRouteByIdService(params?.id)
+        res.status(200).json(response)
+    } catch (err) {
+        res.status(500).json({ message: 'Internal error.' })
+    }
 }
 
 const getAllController = async (_: Request, res: Response) => {
-    const response = await getAllMapRouteService()
-    res.status(200).json({ response })
+    try {
+        const response = await getAllMapRouteService()
+        res.status(200).json({ response })
+    } catch (err) {
+        res.status(500).json({ message: 'Internal error.' })
+    }
 }
 
 const createMapRouteController = async (req: Request, res: Response) => {
-    const response = await createMapRouteService(req)
-    res.status(201).json({ response })
+    try {
+        const response = await createMapRouteService(req)
+        res.status(201).json({ response })
+    } catch (err) {
+        res.status(500).json({ message: 'Internal error.' })
+    }
 }
 
 export { getAllController, getByIdController, createMapRouteController }
