0aea1927c95ca2f0b714a4cc17d263bd67069868
Sagillaire <Jasantise@outlook.es>
2023-09-14 23:52:15 -0500

    Feature: Add trycatch validation to referred controllers

--- src/controllers/referred.controller.ts
+++ src/controllers/referred.controller.ts
@@ -2,18 +2,30 @@ import { Request, Response } from "express";
 import { createReferredService, getAllReferredService, getReferredByIdService } from "../services/referred.service";
 
 const getByIdController = async ({ params }: Request, res: Response) => {
-    const response = await getReferredByIdService(params?.code)
-    res.status(200).json({ response })
+    try {
+        const response = await getReferredByIdService(params?.code)
+        res.status(200).json({ response })
+    } catch (err) {
+        res.status(500).json({ message: 'Internal error.' })
+    }
 }
 
 const getAllController = async (_: Request, res: Response) => {
-    const response = await getAllReferredService()
-    res.status(200).json({ response })
+    try {
+        const response = await getAllReferredService()
+        res.status(200).json({ response })
+    } catch (err) {
+        res.status(500).json({ message: 'Internal error.' })
+    }
 }
 
 const createReferredController = async ({ body }: Request, res: Response) => {
-    const response = await createReferredService(body)
-    res.status(201).json({ response })
+    try {
+        const response = await createReferredService(body)
+        res.status(201).json({ response })
+    } catch (err) {
+        res.status(500).json({ message: 'Internal error.' })
+    }
 }
 
 export { getAllController, getByIdController, createReferredController }
