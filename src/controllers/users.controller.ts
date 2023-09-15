053e78af235ba2812ab66a2340e1590466619c70
Sagillaire <Jasantise@outlook.es>
2023-09-14 23:51:21 -0500

    Feature: Add trycatch validation to user controllers

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -2,13 +2,21 @@ import { Request, Response } from "express";
 import { getUserByIdService, getUsersService } from "../services/users.service";
 
 const getUserByIdController = async ({ params }: Request, res: Response) => {
-    const response = await getUserByIdService(params?.id)
-    res.status(200).json(response)
+    try {
+        const response = await getUserByIdService(params?.id)
+        res.status(200).json(response)
+    } catch (err) {
+        res.status(500).json({ message: 'Internal error.' })
+    }
 }
 
 const getUsersController = async (_: Request, res: Response) => {
-    const response = await getUsersService()
-    res.status(200).json({ response })
+    try {
+        const response = await getUsersService()
+        res.status(200).json({ response })
+    } catch (err) {
+        res.status(500).json({ message: 'Internal error.' })
+    }
 }
 
 export { getUserByIdController, getUsersController }
