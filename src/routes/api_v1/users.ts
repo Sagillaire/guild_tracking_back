62a25bc342a5330971fd2d6c60a4ff95bfecad46
Sagillaire <Jasantise@outlook.es>
2023-09-23 23:55:58 -0500

    Feature: User routes docs

--- src/routes/api_v1/users.ts
+++ src/routes/api_v1/users.ts
@@ -1,11 +1,39 @@
 import { Router } from "express";
 import { getUserByIdController, getUsersController, updateUserCtl, updateUserStateCtl } from "../../controllers/users.controller";
 
-const router = Router()
+// Create a new Express router.
+const router = Router();
 
-router.get('/', getUsersController)
-router.get('/:id', getUserByIdController)
-router.patch('/:id', updateUserCtl)
-router.patch('/state/:id', updateUserStateCtl)
+/**
+ * Route to get a list of users.
+ *
+ * @route GET /users
+ */
+router.get('/', getUsersController);
 
-export { router }
+/**
+ * Route to get a user by their ID.
+ *
+ * @route GET /users/:id
+ * @param {string} id - The ID of the user to retrieve.
+ */
+router.get('/:id', getUserByIdController);
+
+/**
+ * Route to update user information by their ID.
+ *
+ * @route PATCH /users/:id
+ * @param {string} id - The ID of the user to update.
+ * @param {object} body - The updated user data.
+ */
+router.patch('/:id', updateUserCtl);
+
+/**
+ * Route to update the state of a user by their ID.
+ *
+ * @route PATCH /users/state/:id
+ * @param {string} id - The ID of the user whose state to update.
+ */
+router.patch('/state/:id', updateUserStateCtl);
+
+export { router };
