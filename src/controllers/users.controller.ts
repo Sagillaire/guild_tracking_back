2fbcf03599d3dbeaa9d20435987733e53a84ea57
Sagillaire <Jasantise@outlook.es>
2023-09-23 23:54:33 -0500

    Feature: User controllers docs

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,40 +1,73 @@
 import { Request, Response } from "express";
 import { getUserByIdService, getUsersService, updateUserService, updateUserStateService } from "../services/users.service";
 
-const getUserByIdController = async ({ params }: Request, res: Response) => {
+/**
+ * Get a user by their ID.
+ *
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ * @returns {Promise<void>}
+ */
+const getUserByIdController = async ({ params }: Request, res: Response): Promise<void> => {
     try {
-        const response = await getUserByIdService(params?.id)
-        res.status(200).json(response)
+        const response = await getUserByIdService(params?.id);
+        res.status(200).json(response);
     } catch (err) {
-        res.status(500).json({ message: 'Internal error.' })
+        res.status(500).json({ message: 'Internal error.' });
     }
-}
+};
 
-const getUsersController = async (_: Request, res: Response) => {
+/**
+ * Get a list of users.
+ *
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ * @returns {Promise<void>}
+ */
+const getUsersController = async (_: Request, res: Response): Promise<void> => {
     try {
-        const response = await getUsersService()
-        res.status(200).json({ response })
+        const response = await getUsersService();
+        res.status(200).json({ response });
     } catch (err) {
-        res.status(500).json({ message: 'Internal error.' })
+        res.status(500).json({ message: 'Internal error.' });
     }
-}
+};
 
-const updateUserStateCtl = async ({ params }: Request, res: Response) => {
+/**
+ * Update the state of a user by their ID.
+ *
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ * @returns {Promise<void>}
+ */
+const updateUserStateCtl = async ({ params }: Request, res: Response): Promise<void> => {
     try {
-        const response = await updateUserStateService(params?.id)
-        res.status(200).json({ response })
+        const response = await updateUserStateService(params?.id);
+        res.status(200).json({ response });
     } catch (err) {
-        res.status(500).json({ message: 'Internal error.' })
+        res.status(500).json({ message: 'Internal error.' });
     }
-}
+};
 
-const updateUserCtl = async ({ params, body }: Request, res: Response) => {
+/**
+ * Update user information by their ID.
+ *
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ * @returns {Promise<void>}
+ */
+const updateUserCtl = async ({ params, body }: Request, res: Response): Promise<void> => {
     try {
-        const response = await updateUserService(params?.id, body)
-        res.status(200).json({ response })
+        const response = await updateUserService(params?.id, body);
+        res.status(200).json({ response });
     } catch (err) {
-        res.status(500).json({ message: 'Internal error.' })
+        res.status(500).json({ message: 'Internal error.' });
     }
-}
+};
 
-export { getUserByIdController, getUsersController, updateUserStateCtl, updateUserCtl }
+export {
+    getUserByIdController,
+    getUsersController,
+    updateUserStateCtl,
+    updateUserCtl
+};
