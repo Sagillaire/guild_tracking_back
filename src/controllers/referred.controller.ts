37347f26a409874175f09154833db6fa1eb0cc56
Sagillaire <Jasantise@outlook.es>
2023-09-24 22:27:24 -0500

    Feature: referred controllers docs

--- src/controllers/referred.controller.ts
+++ src/controllers/referred.controller.ts
@@ -1,31 +1,52 @@
 import { Request, Response } from "express";
 import { createReferredService, getAllReferredService, getReferredByIdService } from "../services/referred.service";
 
-const getByIdController = async ({ params }: Request, res: Response) => {
+/**
+ * Get a referred entity by its code.
+ *
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ * @returns {Promise<void>}
+ */
+const getByIdController = async ({ params }: Request, res: Response): Promise<void> => {
     try {
-        const response = await getReferredByIdService(params?.code)
-        res.status(200).json({ response })
+        const response = await getReferredByIdService(params?.code);
+        res.status(200).json({ response });
     } catch (err) {
-        res.status(500).json({ message: 'Internal error.' })
+        res.status(500).json({ message: 'Internal error.' });
     }
 }
 
-const getAllController = async (_: Request, res: Response) => {
+/**
+ * Get all referred entities.
+ *
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ * @returns {Promise<void>}
+ */
+const getAllController = async (_: Request, res: Response): Promise<void> => {
     try {
-        const response = await getAllReferredService()
-        res.status(200).json({ response })
+        const response = await getAllReferredService();
+        res.status(200).json({ response });
     } catch (err) {
-        res.status(500).json({ message: 'Internal error.' })
+        res.status(500).json({ message: 'Internal error.' });
     }
 }
 
-const createReferredController = async ({ body }: Request, res: Response) => {
+/**
+ * Create a new referred entity.
+ *
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ * @returns {Promise<void>}
+ */
+const createReferredController = async ({ body }: Request, res: Response): Promise<void> => {
     try {
-        const response = await createReferredService(body)
-        res.status(201).json({ response })
+        const response = await createReferredService(body);
+        res.status(201).json({ response });
     } catch (err) {
-        res.status(500).json({ message: 'Internal error.' })
+        res.status(500).json({ message: 'Internal error.' });
     }
 }
 
-export { getAllController, getByIdController, createReferredController }
+export { getAllController, getByIdController, createReferredController };
