ce6616413029c28399ffb2456ecc029c4d2e9170
Sagillaire <Jasantise@outlook.es>
2023-09-24 22:26:39 -0500

    Feature: referred routes docs

--- src/routes/api_v1/referred.ts
+++ src/routes/api_v1/referred.ts
@@ -3,10 +3,30 @@ import { validatorJoi } from "../../middlewares";
 import { ReferredSchema } from "../../validators/invitation_code.validator";
 import { getByIdController, getAllController, createReferredController } from "../../controllers/referred.controller";
 
-const router = Router()
+// Create a new Express router.
+const router = Router();
 
-router.get('/', getAllController)
-router.get('/:code', getByIdController)
-router.post('/', validatorJoi(ReferredSchema), createReferredController)
+/**
+ * Route to get all referred entities.
+ *
+ * @route GET /referred
+ */
+router.get('/', getAllController);
 
-export { router }
+/**
+ * Route to get a referred entity by its code.
+ *
+ * @route GET /referred/:code
+ * @param {string} code - The code of the referred entity to retrieve.
+ */
+router.get('/:code', getByIdController);
+
+/**
+ * Route to create a new referred entity.
+ *
+ * @route POST /referred
+ * @middleware validatorJoi(ReferredSchema) - Middleware to validate the request body against the ReferredSchema.
+ */
+router.post('/', validatorJoi(ReferredSchema), createReferredController);
+
+export { router };
