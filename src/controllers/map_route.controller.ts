82a922aa87ab0323bcdc34481de45775ef2a1a6a
Sagillaire <Jasantise@outlook.es>
2023-09-25 23:54:07 -0500

    Feature: map_routes controllers docs

--- src/controllers/map_route.controller.ts
+++ src/controllers/map_route.controller.ts
@@ -1,31 +1,52 @@
 import { Request, Response } from "express";
 import { getAllMapRouteService, createMapRouteService, getMapRouteByIdService } from "../services/map_route.service";
 
-const getByIdController = async ({ params }: Request, res: Response) => {
+/**
+ * Get a map route by its ID.
+ *
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ * @returns {Promise<void>}
+ */
+const getByIdController = async ({ params }: Request, res: Response): Promise<void> => {
     try {
-        const response = await getMapRouteByIdService(params?.id)
-        res.status(200).json(response)
+        const response = await getMapRouteByIdService(params?.id);
+        res.status(200).json(response);
     } catch (err) {
-        res.status(500).json({ message: 'Internal error.' })
+        res.status(500).json({ message: 'Internal error.' });
     }
 }
 
-const getAllController = async (_: Request, res: Response) => {
+/**
+ * Get all map routes.
+ *
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ * @returns {Promise<void>}
+ */
+const getAllController = async (_: Request, res: Response): Promise<void> => {
     try {
-        const response = await getAllMapRouteService()
-        res.status(200).json({ response })
+        const response = await getAllMapRouteService();
+        res.status(200).json({ response });
     } catch (err) {
-        res.status(500).json({ message: 'Internal error.' })
+        res.status(500).json({ message: 'Internal error.' });
     }
 }
 
-const createMapRouteController = async (req: Request, res: Response) => {
+/**
+ * Create a new map route.
+ *
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ * @returns {Promise<void>}
+ */
+const createMapRouteController = async (req: Request, res: Response): Promise<void> => {
     try {
-        const response = await createMapRouteService(req)
-        res.status(201).json({ response })
+        const response = await createMapRouteService(req);
+        res.status(201).json({ response });
     } catch (err) {
-        res.status(500).json({ message: 'Internal error.' })
+        res.status(500).json({ message: 'Internal error.' });
     }
 }
 
-export { getAllController, getByIdController, createMapRouteController }
+export { getAllController, getByIdController, createMapRouteController };
