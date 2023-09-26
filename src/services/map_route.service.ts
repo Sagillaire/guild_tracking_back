7b0328b25c563dc43f00a63afef78934e4d234ef
Sagillaire <Jasantise@outlook.es>
2023-09-25 23:55:03 -0500

    Feature: map_routes services docs

--- src/services/map_route.service.ts
+++ src/services/map_route.service.ts
@@ -1,30 +1,47 @@
-import MapRouteModel from "../models/map_route.model"
-import { IMapRoute } from "../interfaces/map_route.interface "
-import { Request } from "express"
-import { decodeToken } from "../utils/jwt.handle"
+import { Request } from "express";
+import { decodeToken } from "../utils/jwt.handle";
+import MapRouteModel from "../models/map_route.model";
+import { IMapRoute } from "../interfaces/map_route.interface ";
 
-const getMapRouteByIdService = async (id: string) => {
-    if (id === null || id === 'undefined') return 'INVALID_ID'
-    const response = await MapRouteModel.findOne({ _id: id }).exec()
-    if (!response) return 'MAP_ROUTE_NOT_FOUND'
+/**
+ * Get a map route by its ID.
+ *
+ * @param {string} id - The ID of the map route to retrieve.
+ * @returns {Promise<any>} - A promise that resolves to the map route or 'INVALID_ID' if the ID is invalid or 'MAP_ROUTE_NOT_FOUND' if not found.
+ */
+const getMapRouteByIdService = async (id: string): Promise<any> => {
+    if (id === null || id === 'undefined') return 'INVALID_ID';
+    const response = await MapRouteModel.findOne({ _id: id }).exec();
+    if (!response) return 'MAP_ROUTE_NOT_FOUND';
 
-    return response
+    return response;
 }
 
-const getAllMapRouteService = async () => {
-    const response = await MapRouteModel.find().exec()
+/**
+ * Get all map routes.
+ *
+ * @returns {Promise<any[]>} - A promise that resolves to an array of map routes.
+ */
+const getAllMapRouteService = async (): Promise<any[]> => {
+    const response = await MapRouteModel.find().exec();
 
-    return response
+    return response;
 }
 
-const createMapRouteService = async ({ body, headers }: Request) => {
-    const { route_info } = body as IMapRoute
-    const { authorization } = headers
+/**
+ * Create a new map route.
+ *
+ * @param {Request} req - The request object.
+ * @returns {Promise<any>} - A promise that resolves to the created map route.
+ */
+const createMapRouteService = async ({ body, headers }: Request): Promise<any> => {
+    const { route_info } = body as IMapRoute;
+    const { authorization } = headers;
 
-    const created_by = await decodeToken(authorization?.split(' ')[1] as string)
-    const response = await MapRouteModel.create({ created_by, route_info })
+    const created_by = await decodeToken(authorization?.split(' ')[1] as string);
+    const response = await MapRouteModel.create({ created_by, route_info });
 
-    return response
+    return response;
 }
 
-export { getMapRouteByIdService, getAllMapRouteService, createMapRouteService }
+export { getMapRouteByIdService, getAllMapRouteService, createMapRouteService };
