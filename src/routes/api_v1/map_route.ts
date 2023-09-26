24fceebce209fcbd7c87da4bb8090ef7c80091d9
Sagillaire <Jasantise@outlook.es>
2023-09-25 23:53:26 -0500

    Feature: map_routes routes docs

--- src/routes/api_v1/map_route.ts
+++ src/routes/api_v1/map_route.ts
@@ -3,10 +3,30 @@ import { validatorJoi } from "../../middlewares";
 import { MapRouteValidation } from "../../validators/map_route.validator";
 import { createMapRouteController, getAllController, getByIdController } from "../../controllers/map_route.controller";
 
-const router = Router()
+// Create a new Express router.
+const router = Router();
 
-router.get('/', getAllController)
-router.get('/:id', getByIdController)
-router.post('/', validatorJoi(MapRouteValidation), createMapRouteController)
+/**
+ * Route to get all map routes.
+ *
+ * @route GET /map-routes
+ */
+router.get('/', getAllController);
 
-export { router }
+/**
+ * Route to get a map route by its ID.
+ *
+ * @route GET /map-routes/:id
+ * @param {string} id - The ID of the map route to retrieve.
+ */
+router.get('/:id', getByIdController);
+
+/**
+ * Route to create a new map route.
+ *
+ * @route POST /map-routes
+ * @middleware validatorJoi(MapRouteValidation) - Middleware to validate the request body against the MapRouteValidation schema.
+ */
+router.post('/', validatorJoi(MapRouteValidation), createMapRouteController);
+
+export { router };
