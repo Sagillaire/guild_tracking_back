94b5c63270b5f5a6dc98894e62e229206d2ff00d
Sagillaire <Jasantise@outlook.es>
2023-09-27 23:28:39 -0500

    Feature: auth route docs

--- src/routes/api_v1/auth.ts
+++ src/routes/api_v1/auth.ts
@@ -3,10 +3,30 @@ import { validatorJoi } from "../../middlewares";
 import { LoginSchema, RegisterSchema } from "../../validators/auth.validator";
 import { loginController, registerController, verifySessionController } from "../../controllers/auth.controller";
 
-const router = Router()
+// Create a new Express router.
+const router = Router();
 
-router.post('/login', validatorJoi(LoginSchema), loginController)
-router.post('/register', validatorJoi(RegisterSchema), registerController)
-router.post('/verifySession', verifySessionController)
+/**
+ * Route to log in a user.
+ *
+ * @route POST /auth/login
+ * @middleware validatorJoi(LoginSchema) - Middleware to validate the request body against the LoginSchema.
+ */
+router.post('/login', validatorJoi(LoginSchema), loginController);
 
-export { router }
+/**
+ * Route to register a new user.
+ *
+ * @route POST /auth/register
+ * @middleware validatorJoi(RegisterSchema) - Middleware to validate the request body against the RegisterSchema.
+ */
+router.post('/register', validatorJoi(RegisterSchema), registerController);
+
+/**
+ * Route to verify a user's session.
+ *
+ * @route POST /auth/verifySession
+ */
+router.post('/verifySession', verifySessionController);
+
+export { router };
