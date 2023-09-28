0ba72b1acf3284841384f470bb6ff3c7962355b4
Sagillaire <Jasantise@outlook.es>
2023-09-27 23:27:59 -0500

    Feature: auth controller docs

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,42 +1,60 @@
 import { Request, Response } from "express";
-import { loginUser, registerNewUser, verifySessionService } from "../services/auth.service";
 import { handleCustomResponse } from "../utils/error.handle";
+import { loginUser, registerNewUser, verifySessionService } from "../services/auth.service";
 
-const loginController = async ({ body }: Request, res: Response) => {
+/**
+ * Controller for user login.
+ *
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ */
+const loginController = async ({ body }: Request, res: Response): Promise<void> => {
     try {
-        const response = await loginUser(body)
+        const response = await loginUser(body);
         if (['INCORRECT_PASSWORD', 'USER_NOT_FOUND'].includes(response as string)) {
-            handleCustomResponse(res, 403, response as string)
+            handleCustomResponse(res, 403, response as string);
         } else {
-            handleCustomResponse(res, 200, 'Success', response)
+            handleCustomResponse(res, 200, 'Success', response);
         }
     } catch (err) {
-        res.status(500).json({ message: 'Internal error.' })
+        res.status(500).json({ message: 'Internal error.' });
     }
 }
 
-const registerController = async ({ body }: Request, res: Response) => {
+/**
+ * Controller for user registration.
+ *
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ */
+const registerController = async ({ body }: Request, res: Response): Promise<void> => {
     try {
-        const response = await registerNewUser(body)
+        const response = await registerNewUser(body);
         if (['USER_DOESNT_MATCH_GUILD', 'USER_INVALID_ALBION_DB',
             'USERNAME_ALREADY_EXISTS', 'INVALID_CODE'].includes(response as string)) {
-            handleCustomResponse(res, 403, response as string)
+            handleCustomResponse(res, 403, response as string);
         } else {
-            handleCustomResponse(res, 200, 'Success', response)
+            handleCustomResponse(res, 200, 'Success', response);
         }
     } catch (err) {
-        res.status(500).json({ message: 'Internal error.' })
+        res.status(500).json({ message: 'Internal error.' });
     }
 }
 
-const verifySessionController = async ({ body }: Request, res: Response) => {
+/**
+ * Controller to verify a user's session.
+ *
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ */
+const verifySessionController = async ({ body }: Request, res: Response): Promise<void> => {
     try {
-        const response = await verifySessionService(body)
-        handleCustomResponse(res, 200, 'Success!', response)
+        const response = await verifySessionService(body);
+        handleCustomResponse(res, 200, 'Success!', response);
     } catch (err) {
-        const { message } = err as any
-        handleCustomResponse(res, 401, 'Unauthorized!', message)
+        const { message } = err as any;
+        handleCustomResponse(res, 401, 'Unauthorized!', message);
     }
 }
 
-export { loginController, registerController, verifySessionController }
+export { loginController, registerController, verifySessionController };
