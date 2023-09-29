860d01205ab438e3259a711057c375479856dc2f
Sagillaire <Jasantise@outlook.es>
2023-09-28 21:50:03 -0500

    Feature: session.middleware docs

--- src/middlewares/session.middleware.ts
+++ src/middlewares/session.middleware.ts
@@ -1,21 +1,27 @@
+import { verifyToken } from "../utils/jwt.handle";
 import { NextFunction, Request, Response } from "express";
 import { handleCustomResponse } from "../utils/error.handle";
-import { verifyToken } from "../utils/jwt.handle";
 
-const checkJwt = async ({ headers }: Request, res: Response, next: NextFunction) => {
+/**
+ * Middleware to check the validity of a JSON Web Token (JWT) in the request headers.
+ *
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ * @param {NextFunction} next - The next function to call if the token is valid.
+ */
+const checkJwt = async ({ headers }: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-        const jwtByUser = headers.authorization || ''
-        const jwt = jwtByUser.split(' ').pop()
+        const jwtByUser = headers.authorization || '';
+        const jwt = jwtByUser.split(' ').pop();
 
-        const isOk = await verifyToken(`${jwt}`)
+        const isOk = await verifyToken(`${jwt}`);
 
-        if (!isOk) return handleCustomResponse(res, 401, 'INVALID_SESSION')
+        if (!isOk) return handleCustomResponse(res, 401, 'INVALID_SESSION');
 
-        next()
-
+        next();
     } catch (err) {
-        handleCustomResponse(res, 400, 'NOT_AUTHORIZED')
+        handleCustomResponse(res, 400, 'NOT_AUTHORIZED');
     }
 }
 
-export { checkJwt }
+export { checkJwt };
