1b8a6eff25467f308f650e49831ac090ff6ab5ee
Sagillaire <Jasantise@outlook.es>
2023-09-28 21:49:09 -0500

    Feature: api route v1  docs

--- src/routes/api_v1/index.ts
+++ src/routes/api_v1/index.ts
@@ -1,18 +1,24 @@
 import { Router } from "express";
-import { readdirSync } from 'fs'
+import { readdirSync } from 'fs';
 
-const routerApiV1 = Router()
-const PATH_ROUTER = `${__dirname}`
+// Create a new Express router for API version 1.
+const routerApiV1 = Router();
 
-const clearFileName = (fileName: string) => fileName.split('.').shift()
+// Define the path to the router modules.
+const PATH_ROUTER = `${__dirname}`;
 
+// Function to remove the file extension from a filename.
+const clearFileName = (fileName: string) => fileName.split('.').shift();
+
+// Read and import all router modules in the current directory.
 readdirSync(PATH_ROUTER).forEach((filename) => {
-    const cleanName = clearFileName(filename)
+    const cleanName = clearFileName(filename);
     if (cleanName !== 'index') {
         import(`./${cleanName}`).then((moduleRouter) => {
-            routerApiV1.use(`/v1/${cleanName}`, moduleRouter.router)
-        })
+            // Mount the imported router under the /v1/cleanName route.
+            routerApiV1.use(`/v1/${cleanName}`, moduleRouter.router);
+        });
     }
-})
+});
 
-export { routerApiV1 } 
+export { routerApiV1 };
