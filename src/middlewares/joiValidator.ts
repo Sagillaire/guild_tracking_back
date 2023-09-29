a3e3b92e70d83a5c5462aa89a054c2dd0749f6d3
Sagillaire <Jasantise@outlook.es>
2023-09-28 21:52:08 -0500

    Feature: joiValidator  docs

--- src/middlewares/joiValidator.ts
+++ src/middlewares/joiValidator.ts
@@ -1,17 +1,23 @@
 import { NextFunction, Request, Response } from "express";
 import { handleCustomResponse } from "../utils/error.handle";
 
-export const validatorJoi = (schema: any) => {
-    return async ({ body }: Request, res: Response, next: NextFunction) => {
+/**
+ * Middleware for validating a request body against a given schema using Joi.
+ *
+ * @param {any} schema - The Joi schema to validate the request body against.
+ * @returns {Function} - A middleware function to handle request validation.
+ */
+export const validatorJoi = (schema: any): Function => {
+    return async ({ body }: Request, res: Response, next: NextFunction): Promise<void> => {
         try {
-            const { error } = await schema.validate(body)
+            const { error } = await schema.validate(body);
             if (error) {
-                handleCustomResponse(res, 400, error.details[0].message)
+                handleCustomResponse(res, 400, error.details[0].message);
             } else {
-                next()
+                next();
             }
         } catch (err) {
-            handleCustomResponse(res, 500, 'InternalError')
+            handleCustomResponse(res, 500, 'InternalError');
         }
     }
 }
