6f2c4d1889e3e36fee9141206cca70b55111aa09
Sagillaire <Jasantise@outlook.es>
2023-09-29 23:37:26 -0500

    Feature: map_route schema docs

--- src/models/map_route.model.ts
+++ src/models/map_route.model.ts
@@ -1,44 +1,73 @@
-import { Schema, model } from 'mongoose'
-import { IMapRoute } from '../interfaces/map_route.interface '
+import { Schema, model } from 'mongoose';
+import { IMapRoute } from '../interfaces/map_route.interface ';
 
-/* Se declara el Schema: El esquema es la representacion de las propiedades que se 
-van a guardar en la base de datos */
+/**
+ * Mongoose schema for the MapRoute entity.
+ */
 const MapRouteSchema = new Schema<IMapRoute>(
     {
+        /**
+         * The user who created the map route.
+         */
         created_by: {
             type: String
         },
+
+        /**
+         * The state of the map route (e.g., active or inactive).
+         */
         state: {
             type: Number,
             default: 1
         },
+
+        /**
+         * Information about the map route, including time, map name, map zone, and state.
+         */
         route_info: [
             {
+                /**
+                 * The time of the map route.
+                 */
                 time: {
                     type: String,
-                    require: true
+                    required: true
                 },
+
+                /**
+                 * The name of the map.
+                 */
                 map_name: {
                     type: String,
-                    require: true
+                    required: true
                 },
+
+                /**
+                 * The zone of the map.
+                 */
                 map_zone: {
                     type: String,
-                    require: true
+                    required: true
                 },
+
+                /**
+                 * The state of the map route.
+                 */
                 state: {
                     type: String,
-                    require: true
+                    required: true
                 }
             }
         ]
     },
     {
-        timestamps: true, // Para que guarde la fecha de creacion y actualizacion
-        versionKey: false // Para que no guarde datos por version
+        timestamps: true,
+        versionKey: false
     }
-)
+);
 
-// Implementamos el Schema en el modelo
-const MapRouteModel = model('map_routes', MapRouteSchema)
-export default MapRouteModel
+/**
+ * Mongoose model for the MapRoute entity.
+ */
+const MapRouteModel = model<IMapRoute>('map_routes', MapRouteSchema);
+export default MapRouteModel;
