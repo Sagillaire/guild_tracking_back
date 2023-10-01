c9e12770b45c6889d1ab825fa78daec534159430
Sagillaire <Jasantise@outlook.es>
2023-09-30 23:20:40 -0500

    Feature: map_route validator docs

--- src/validators/map_route.validator.ts
+++ src/validators/map_route.validator.ts
@@ -1,6 +1,10 @@
 import Joi from "joi";
 import { IMapRoute, IRouteInfo } from "../interfaces/map_route.interface ";
 
+/**
+ * Esquema de validación para la ruta del mapa.
+ * @type {Joi.ObjectSchema<IMapRoute>}
+ */
 export const MapRouteValidation = Joi.object<IMapRoute>({
     route_info: Joi.array().items(Joi.object<IRouteInfo>({
         time: Joi
