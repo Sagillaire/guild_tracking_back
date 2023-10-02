0a920d43a4127b5e836c508c6b8fad5a566bc85b
Sagillaire <Jasantise@outlook.es>
2023-10-01 23:58:32 -0500

    Feature: user interface docs

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,5 +1,8 @@
 import { IAuth } from "./auth.interface";
 
+/**
+ * Interface representing a user, extending the IAuth interface.
+ */
 export interface IUser extends IAuth {
     id:                 string;
     rol:                string;
