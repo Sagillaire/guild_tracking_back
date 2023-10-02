587c92b0cf182f1aa38e51c707b71c23332dc7b6
Sagillaire <Jasantise@outlook.es>
2023-10-01 23:57:38 -0500

    Feature: referred interface docs

--- src/interfaces/referred.interface .ts
+++ src/interfaces/referred.interface .ts
@@ -1,7 +1,9 @@
-
+/**
+ * Interface representing an invitation code.
+ */
 export interface IInvitationCode {
-    id:                 string;
-    code:               string;
-    guild:              string;
-    status:             string;
+    id:             string;
+    code:           string;
+    guild:          string;
+    status:         string;
 }
