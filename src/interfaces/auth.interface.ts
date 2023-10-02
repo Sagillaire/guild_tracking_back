4409f92623e93684ab8d0c9ba5f06fc71b223d6d
Sagillaire <Jasantise@outlook.es>
2023-10-01 23:47:58 -0500

    Feature: auth interface docs

--- src/interfaces/auth.interface.ts
+++ src/interfaces/auth.interface.ts
@@ -1,9 +1,15 @@
+/**
+ * Interface representing user authentication.
+ */
 export interface IAuth {
     code:       string;
     username:   string;
     password:   string;
 }
 
+/**
+ * Interface representing session verification.
+ */
 export interface IVerifySession {
     token:      string;
 }
