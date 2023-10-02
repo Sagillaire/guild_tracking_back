b9dd7d38c0adc9bf2528c2815806e436bdc5f96b
Sagillaire <Jasantise@outlook.es>
2023-10-01 23:56:26 -0500

    Feature: route_map interface docs

--- src/interfaces/map_route.interface .ts
+++ src/interfaces/map_route.interface .ts
@@ -1,16 +1,21 @@
-
+/**
+ * Interface representing a map route.
+ */
 export interface IMapRoute {
-    createdAt:          Date;
-    updatedAt:          Date;
-    state:              number;
-    id:                 string;
-    created_by:         string;
-    route_info:         IRouteInfo[]
+    createdAt:      Date;
+    updatedAt:      Date;
+    state:          number;
+    id:             string;
+    created_by:     string;
+    route_info:     IRouteInfo[];
 }
 
+/**
+ * Interface representing information about a specific route within a map.
+ */
 export interface IRouteInfo {
-    time:               string;
-    state?:             number;
-    map_name:           string;
-    map_zone:           string;
+    time:           string;
+    state?:         number;
+    map_name:       string;
+    map_zone:       string;
 }
