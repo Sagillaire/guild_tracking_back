df6537233f3f1c6f826d6865c312ed213249d37c
Sagillaire <Jasantise@outlook.es>
2023-10-03 23:51:13 -0500

    Feature: getRequest utils docs

--- src/utils/getRequest.ts
+++ src/utils/getRequest.ts
@@ -1,11 +1,17 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from "axios"
 
-// GET REQUESTS
-export const getRequest = async (url: string, config?: AxiosRequestConfig) => {
+/**
+ * Sends a GET request to the specified URL and returns the response data or an error.
+ * @param url - The URL to send the GET request to.
+ * @param config - (Optional) Configuration options for the Axios GET request.
+ * @returns A Promise that resolves to the response data if the request is successful,
+ * or rejects with an error if the request fails.
+ */
+export const getRequest = async (url: string, config?: AxiosRequestConfig): Promise<any> => {
     try {
         const response: AxiosResponse = await axios.get(url, config || {});
-        return response.data
+        return response.data;
     } catch (error) {
-        return error
+        return error;
     }
 }
