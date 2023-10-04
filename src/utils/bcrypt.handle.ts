295b1dccb7bbf5a8c90671c57d9da5f5569520a7
Sagillaire <Jasantise@outlook.es>
2023-10-03 23:52:31 -0500

    Feature: bcript.handle utils docs

--- src/utils/bcrypt.handle.ts
+++ src/utils/bcrypt.handle.ts
@@ -1,13 +1,25 @@
-import { hash, compare } from 'bcryptjs'
+import { hash, compare } from 'bcryptjs';
 
-const encrypt = async (pass: string) => {
-    const passwordHash = await hash(pass, 8)
-    return passwordHash
+/**
+ * Hashes a password using bcrypt.
+ * @param pass - The password to be hashed.
+ * @returns A Promise that resolves to the hashed password.
+ */
+const encrypt = async (pass: string): Promise<string> => {
+    const passwordHash = await hash(pass, 8);
+    return passwordHash;
 }
 
-const verify = async (pass: string, passwordHash: string) => {
-    const isCorrect = compare(pass, passwordHash)
-    return isCorrect
+/**
+ * Verifies a password against a bcrypt hashed password.
+ * @param pass - The password to be verified.
+ * @param passwordHash - The bcrypt hashed password to compare against.
+ * @returns A Promise that resolves to a boolean indicating whether the password is correct.
+ */
+const verify = async (pass: string, passwordHash: string): Promise<boolean> => {
+    const isCorrect = await compare(pass, passwordHash);
+    return isCorrect;
 }
+
 
 export { encrypt, verify }
