d1367ef464514aef64d67d1150f5d97e18c2af0b
Sagillaire <Jasantise@outlook.es>
2023-10-04 22:33:33 -0500

    Feature: config mongo docs

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -1,13 +1,24 @@
-import 'dotenv/config' // Importamos dotenv para hacer uso de las variables de entorno
-import mongoose, { connect } from 'mongoose' // Se usa mongoose para gestionar la conexion con mongo
+import 'dotenv/config'
+import mongoose, { connect } from 'mongoose'
 import { downConnection, readyConnection } from '../utils/dbConsoleStart';
 
-mongoose.set('strictQuery', false); // Para eliminar advertencias en consola
+/**
+ * Configures Mongoose to disable strict mode for queries.
+ * This allows querying fields that are not defined in the schema.
+ */
+mongoose.set('strictQuery', false);
 
+/**
+ * Asynchronously establishes a connection to the MongoDB database using the provided URI.
+ * Logs the "readyConnection" message if the connection is successful.
+ * Logs the "downConnection" message if an error occurs during the connection process.
+ */
 (async (): Promise<void> => {
     try {
-        const DB_URI = <string>process.env.DB_URI // Se obtiene la DB_URI del .env
-        await connect(DB_URI) // Se inicializa la conecion con la base de datos
-        console.log(readyConnection)
-    } catch (err) { console.log(downConnection) }
-})()
+        const DB_URI = <string>process.env.DB_URI;
+        await connect(DB_URI);
+        console.log(readyConnection);
+    } catch (err) {
+        console.log(downConnection);
+    }
+})();
